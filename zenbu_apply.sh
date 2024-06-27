8fa9451330399543a3910d4c88f8745ba62ba229
t-daisuke <t.daisuke.64@gmail.com>
2024-06-27 13:44:43 +0900

    イメージの上げ直しからやり直したが、あんまりうまく行ってないかも

--- zenbu_apply.sh
+++ zenbu_apply.sh
@@ -1,18 +1,19 @@
-# set -eux
-# aws ecr get-login-password --profile haruotsu-profile | docker login --username AWS --password-stdin 905418468932.dkr.ecr.us-east-1.amazonaws.com
+set -eux
+aws ecr get-login-password --profile dos_prf | docker login --username AWS --password-stdin 905418468932.dkr.ecr.us-east-1.amazonaws.com
 
-# docker build -t haruotsu-rails-images:latest ./productRecommendation-system
-# docker build -t haruotsu-flask-images:latest ./recommendationEngine
+docker build -t dos_img_aiu:latest ./ch14_aiu
+docker build -t dos_img_eoa:latest ./ch14_eoa
+
 # # docker　imagesを確認
-# docker images
+docker images
 
 # # 作成したdocker imageをタグ付け
-# docker tag haruotsu-flask-images 905418468932.dkr.ecr.us-east-1.amazonaws.com/haruotsu-flask-images:latest
-# docker tag haruotsu-rails-images 905418468932.dkr.ecr.us-east-1.amazonaws.com/haruotsu-rails-images:latest
+docker tag dos_img_aiu 905418468932.dkr.ecr.us-east-1.amazonaws.com/dos_img_aiu:latest
+docker tag dos_img_eoa 905418468932.dkr.ecr.us-east-1.amazonaws.com/dos_img_eoa:latest
 
 # # ECRにpush
-# docker push 905418468932.dkr.ecr.us-east-1.amazonaws.com/haruotsu-rails-images:latest
-# docker push 905418468932.dkr.ecr.us-east-1.amazonaws.com/haruotsu-flask-images:latest
+docker push 905418468932.dkr.ecr.us-east-1.amazonaws.com/dos_img_aiu:latest
+docker push 905418468932.dkr.ecr.us-east-1.amazonaws.com/dos_img_eoa:latest
 
 # # k8sのデプロイメントをapply
 kubectl apply -f ./aiu-man.yaml
