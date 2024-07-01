13dbea64762310a7b46a81ec9402ad79a367aaca
t-daisuke <t.daisuke.64@gmail.com>
2024-07-01 17:27:01 +0900

    ポートを本番の環境に寄せた

--- ch14_eoa/config/initializers/opentelemetry.rb
+++ ch14_eoa/config/initializers/opentelemetry.rb
@@ -17,7 +17,7 @@ OpenTelemetry::SDK.configure do |c|
   c.add_span_processor(
     OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
       OpenTelemetry::Exporter::OTLP::Exporter.new(
-        endpoint: 'http://localhost:4317' # エンドポイントを適切に設定
+        endpoint: 'http://localhost:3001' # エンドポイントを適切に設定
       )
     )
   )
