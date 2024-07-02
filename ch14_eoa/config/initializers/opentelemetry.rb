22d22531995a75fcc95e1fa334cc44b4e058afd8
t-daisuke <t.daisuke.64@gmail.com>
2024-07-02 10:20:09 +0900

    普通に変えてみた

--- ch14_eoa/config/initializers/opentelemetry.rb
+++ ch14_eoa/config/initializers/opentelemetry.rb
@@ -1,28 +1,8 @@
-# config/initializers/opentelemetry.rb
-
+# ch14_aiu/config/opentelemetry.rb
 require 'opentelemetry/sdk'
 require 'opentelemetry/exporter/otlp'
-require 'opentelemetry/instrumentation/rack'
-require 'opentelemetry/instrumentation/active_record'
-require 'opentelemetry/instrumentation/action_pack'
-require 'opentelemetry/instrumentation/action_view'
-require 'opentelemetry/instrumentation/active_support'
 
-# SDKの設定
 OpenTelemetry::SDK.configure do |c|
-  # インストルメンテーションの設定
-  c.use 'OpenTelemetry::Instrumentation::Rack'
-  c.use 'OpenTelemetry::Instrumentation::ActiveRecord'
-  c.use 'OpenTelemetry::Instrumentation::ActionPack'
-  c.use 'OpenTelemetry::Instrumentation::ActionView'
-  c.use 'OpenTelemetry::Instrumentation::ActiveSupport'
-
-  # OTLPエクスポーターの設定
-  c.add_span_processor(
-    OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(
-      OpenTelemetry::Exporter::OTLP::Exporter.new(
-        endpoint: ENV['OTEL_EXPORTER_OTLP_ENDPOINT']
-      )
-    )
-  )
+  c.service_name = 'service-b'
+  c.use_all
 end
